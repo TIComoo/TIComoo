a416eecff881cb67e7324eaed9bcde2b3a011d19
sergiogmoraleda <91477608+sergiogmoraleda@users.noreply.github.com>
2022-10-18 18:59:20 +0200

    rama main correcta

--- app/src/test/java/g5/app/AppApplicationTests.java
+++ app/src/test/java/g5/app/AppApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package g5.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class AppApplicationTests {
 
 	@Test
 	void contextLoads() {
