21abe1cdbbdefacd86760a3e610032dc6ab066d1
rubafdez11 <ruben.fernanz1@alu.uclm.es>
2022-10-19 21:14:16 +0200

    Plato Service y controller terminado

--- app/src/main/java/g5/app/PlatoController.java
+++ app/src/main/java/g5/app/PlatoController.java
@@ -1,5 +1,52 @@
 package g5.app;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.json.JSONObject;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
+@RestController
+@RequestMapping("plato")
 public class PlatoController {
+
+    @Autowired
+    PlatoService platoService=new PlatoService();
     
+    @PostMapping("/insert")
+	public void register(@RequestBody Map<String, Object> info) {
+		try {
+			JSONObject jso = new JSONObject(info);
+			platoService.insertPlato(jso);
+
+		} catch (Exception e) {
+			throw new ResponseStatusException(HttpStatus.FORBIDDEN, e.getMessage());
+		}
+	}
+
+    @PostMapping("/update")
+	public void update(@RequestBody Map<String, Object> info) {
+		try {
+			JSONObject jso = new JSONObject(info);
+			platoService.updatePlato(jso);
+
+		} catch (Exception e) {
+			throw new ResponseStatusException(HttpStatus.FORBIDDEN, e.getMessage());
+		}
+	}
+
+    @PostMapping("/delete")
+	public void delete(@RequestBody Map<String, Object> info) {
+		try {
+			JSONObject jso = new JSONObject(info);
+            
+			platoService.deletePlato(jso);
+
+		} catch (Exception e) {
+			throw new ResponseStatusException(HttpStatus.FORBIDDEN, e.getMessage());
+		}
+	}
 }
--- app/src/main/java/g5/app/PlatoService.java
+++ app/src/main/java/g5/app/PlatoService.java
@@ -72,23 +72,84 @@ public class PlatoService {
         }
             
         platoRepository.insert(plato);
-        
-
-        
 
        
     }
     
-    public void updatePlato(Plato plato){
-       
+    public void updatePlato(JSONObject json)throws Exception{
+        
+        String id=json.getString("id");
+        String nombre=json.getString("nombre");
+        String imagen=json.getString("imagen");
+        String descripcion=json.getString("descripcion");
+        String precio=json.getString("precio");
+        String aptoVeganos=json.getString("aptoVeganos");
+        String categoria=json.getString("categoria");
+
+        if(id.isEmpty()||nombre.isEmpty()||imagen.isEmpty()||descripcion.isEmpty()||precio.isEmpty()||aptoVeganos.isEmpty()||categoria.isEmpty()){
+            throw new Exception("Campos vacios");
+        
+        }
+
+        try {
+            Double.parseDouble(precio);
+            
+        } catch (NumberFormatException e){
+            throw new Exception("El precio no es un dato numerico");
+        }
+        
+        if(!aptoVeganos.equalsIgnoreCase("si") && !aptoVeganos.equalsIgnoreCase("no")){
+            throw new Exception("El valor de apto veganos debe de ser si o no");
+        }
+
+        if(!categoria.equalsIgnoreCase("primero") && !categoria.equalsIgnoreCase("segundo") && !categoria.equalsIgnoreCase("postre")){
+            throw new Exception("La categoria del plato debe ser valida: Primero,segundo o postre");
+        }
+
+        if (platoRepository.findBynombre(nombre).isPresent()){
+            throw new Exception("El plato ya existe en el sistema");
+        }
+
+        try{
+            if (platoRepository.findById(Integer.parseInt(id)).isPresent()){
+                throw new Exception("El plato ya existe en el sistema");
+            }
+            
+        }catch(NumberFormatException e){
+            throw new Exception("El id no es un dato numerico");
+        }
+                
+        Plato plato =new Plato();
+
+        plato.setId(Integer.parseInt(id));
+        plato.setCategoria(categoria);
+        plato.setDescripcion(descripcion);
+        plato.setNombre(nombre);
+        plato.setPrecio(Double.parseDouble(precio));
+        plato.setImagen(imagen);
+
+        if(aptoVeganos.equalsIgnoreCase("si")){
+
+            plato.setAptoVeganos(true); 
+
+        }else{
+            plato.setAptoVeganos(false); 
+        }
+            
         platoRepository.save(plato);
+       
+       
 
     }
 
-    public void deletePlato(int id)throws NullPointerException{
+    public void deletePlato(JSONObject json)throws Exception{
         
-        Plato plato=platoRepository.findById(id).orElseThrow(NullPointerException::new);
-        platoRepository.delete(plato);
+        String id=json.getString("id");
+        int Id=Integer.parseInt(id);
+        
+        platoRepository.delete(platoRepository.findById(Id).get());
+            
+       
         
     }
 }
