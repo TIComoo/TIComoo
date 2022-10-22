fe3621b7a7876a9d4f2cd7c4a2ad2e001215875d
barchi24 <gregorio.barchino@alu.uclm.es>
2022-10-22 16:49:14 +0200

    Adición de comentarios al código

--- app/src/main/java/g5/app/EncoderConfig.java
+++ app/src/main/java/g5/app/EncoderConfig.java
@@ -4,6 +4,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+/*********************************************************************
+*
+* Class Name: EncoderConfig
+* Author/s name: Gregorio
+* Release/Creation date: 19/10/2022
+* Class description: Devuelve un encriptador de contraseñas. Se utiliza en WebSecurityConfig
+*
+**********************************************************************
+*/
+
 @Configuration
 public class EncoderConfig {
     
