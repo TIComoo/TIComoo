a18dca9db737bc76280279d5483ff01e74f7c266
Uve95 <MVictoria.Alcazar@alu.uclm.es>
2022-10-22 13:12:14 +0200

    Update RestauranteTest.java

--- app/src/test/java/g5/app/RestauranteTest.java
+++ app/src/test/java/g5/app/RestauranteTest.java
@@ -12,7 +12,7 @@ public class RestauranteTest {
     
     String nombre="Domino's ";
     String razon="Pizzerias Ciudad Real";
-    String CIF="1234567CJS";
+    String CIF="1234567CJSh";
     String direccion="C/Alegria";
     Integer tlf=67676767;
     String categoria="Pizzeria";
