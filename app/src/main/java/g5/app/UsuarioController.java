f4ab51d67a8ec7f0064d948d2e975c9c4b4fbc38
sergiogmoraleda <sergio.garcia52@alu.uclm.es>
2022-10-24 19:24:31 +0200

    primeros pasos controller

--- app/src/main/java/g5/app/UsuarioController.java
+++ app/src/main/java/g5/app/UsuarioController.java
@@ -1,5 +1,35 @@
 package g5.app;
 
+
+
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuario")
 public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @PostMapping(value = "/crearUsuario", consumes = {"application/json"})
+    public void crearUsuario(@RequestBody Usuario usuario) {
+        
+       /*  String email = jsonObject.getString("email");
+        String nombre = jsonObject.getString("nombre");
+        String apellido = jsonObject.getString("apellido");
+        String nif = jsonObject.getString("nif");
+        String direccion = jsonObject.getString("direccion");
+        int telefono = jsonObject.getInt("telefono");
+        String pwd = jsonObject.getString("pwd");
+ */
+        //Usuario usuario = new Usuario(email, nombre, apellido, nif, direccion, telefono, pwd);
+
+        this.usuarioService.crearUsuario(usuario);
+    }
     
 }
