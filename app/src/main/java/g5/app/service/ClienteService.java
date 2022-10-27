d1856dc79797980fb3cdf9f521d407d0512ebcea
barchi24 <91192166+barchi24@users.noreply.github.com>
2022-10-27 12:13:36 +0200

    Update ClienteService.java

--- app/src/main/java/g5/app/service/ClienteService.java
+++ app/src/main/java/g5/app/service/ClienteService.java
@@ -13,11 +13,12 @@ import g5.app.model.Cliente;
 @Service
 public class ClienteService {
     @Autowired
-    ClienteRepository usuarioRepository;
+    ClienteRepository usuarioRepository; // diría de cambiarle el nombre a clienteRepository, o incluso cambiarlo a repository o a dao para todas las clases,
+                                         // ya que se sobreentiende que es del tipo de usuario que es, y queda más corto
     
-
+    // habría que cambiar el nombre de los métodos de Usuario a Cliente
     //CRUD Crear con TDD.
-    public Cliente crearUsuario(Cliente cliente) {
+    public Cliente crearUsuario(Cliente cliente) { // diría de cambiar el nombre del método que usa el save() a guardarCliente, guardarUsuario, guardarRider, etc
         return usuarioRepository.save(cliente);
     }
 
@@ -33,6 +34,8 @@ public class ClienteService {
         usuarios = usuarioRepository.findAll();
 
         return usuarios;
+        
+        // Aquí por qué no hacer directamente return clienteRepository.findAll(); y se queda en una línea
     }
 
     
