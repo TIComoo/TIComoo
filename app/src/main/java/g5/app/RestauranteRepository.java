90f805394e8b7a0cbf9dac855237c952f08e8d30
Uve95 <91894766+Uve95@users.noreply.github.com>
2022-10-27 15:37:37 +0200

    Add files via upload

---
+++ app/src/main/java/g5/app/RestauranteRepository.java
@@ -0,0 +1,11 @@
+package g5.app;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.*;
+public interface RestauranteRepository extends MongoRepository<Restaurante,String>{
+
+	Optional<Restaurante> findByemail(String email);
+	Optional<Restaurante> findBynombre(String nombre);
+
+
+}
