6ca1b94b64efe592bf80b87e627600435fe9c4da
barchi24 <gregorio.barchino@alu.uclm.es>
2022-10-29 12:49:59 +0200

    Frontend

--- app/src/main/java/g5/app/controller/UsuarioController.java
+++ app/src/main/java/g5/app/controller/UsuarioController.java
@@ -1,12 +1,14 @@
 package g5.app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import g5.app.service.UsuarioService;
 
 //@RestController
 //@RequestMapping("/user") a lo mejor habría que poner algo así
+@Controller
 public class UsuarioController {
 
 	@Autowired
