a7adbb84fe617ab2b3686d4bb8404d7dcf364cea
Uve95 <91894766+Uve95@users.noreply.github.com>
2022-11-09 16:36:10 +0100

    Delete server directory

--- server/src/main/java/ticomo/app/dao/AdministradorRepository.java
+++
@@ -1,9 +0,0 @@
-package ticomo.app.dao;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import ticomo.app.model.Administrador;
-
-public interface AdministradorRepository extends MongoRepository<Administrador, String> {
-
-}
