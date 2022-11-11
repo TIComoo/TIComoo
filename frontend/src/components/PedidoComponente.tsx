cd1e2c2ef1a091c5b0261660005e165f54eabb9b
Nacho009 <72605872+Nacho009@users.noreply.github.com>
2022-11-11 11:30:29 +0100

    Backend pedido terminado, interfaz pedido en curso

---
+++ frontend/src/components/PedidoComponente.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import {IPedido} from "../App";
+import {Row, Col, Button} from "react-bootstrap";
+
+
+interface IProps {
+    pedido_:  IPedido
+    setPedido:  React.Dispatch<React.SetStateAction<IPedido[]>>
+}
+
+
+const PedidoComponente: React.FC<IProps> = ({ pedido_, setPedido }) => {
+   
+    const deletePedido = () => {
+        const options = { method: 'DELETE' }
+
+        fetch(`http://localhost:8080/pedido/${pedido_.id}`, options)
+            .then(response => {
+                if (response.ok) {
+                    setPedido(prevState => prevState.filter(item => item.id !== pedido_.id))
+                }
+            })
+            .catch(error => {
+                console.log(error)
+                alert("No se elimina")
+            })
+    }
+
+    return (
+        <Row>
+
+            <Col md={8}>
+                <span>{pedido_.id}</span>
+            </Col>
+            <Col md={2}>
+                <Button onClick={deletePedido}>X</Button>
+            </Col>
+        </Row>
+    )
+}
+
+export default PedidoComponente;
